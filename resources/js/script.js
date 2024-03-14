444cc761d2daf859e36f179e5142faf475919b05
Valerio-Catellani <valeriocatellani@gmail.com>
2024-03-14 11:36:52 +0100

    first snack

---
+++ resources/js/script.js
@@ -0,0 +1,29 @@
+const userInput = document.getElementById("user-input");
+const sendButton = document.getElementById("send-button");
+const response = document.getElementById("response");
+
+
+
+
+let array = [];
+let sum = 0;
+
+while (sum < 50) {
+    let promptInput = parseInt(prompt("inserisci un numero"))
+    array.push(promptInput);
+    sum += promptInput;
+}
+
+
+
+
+// sendButton.addEventListener('click', function () {
+//   let number = parseInt(userInput.value);
+//   array.push(number)
+//   while()
+// })
+
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array.
+// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+
